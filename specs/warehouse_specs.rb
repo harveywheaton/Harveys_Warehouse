490d725d3c7b4c1ccf15d3aca184eb9a1127c594
Harvey Wheaton <harvey.wheaton@codeclan.com>
2016-02-20 10:02:18 +0000

    Adds weight data to items in bays and function to add up weight for items in a list of bays

--- specs/warehouse_specs.rb
+++ specs/warehouse_specs.rb
@@ -5,44 +5,44 @@ require ('minitest/autorun')
 require_relative ('../warehouse_functions')
 
 
-
 class TestMyFunctions < MiniTest::Test
 
 #------------------------------------------
+# Set up all the warehouse data
 def setup
  
     @warehouse_data = {
 
-      b7: {item: "bath fizzers", position: 27},
-      a3: {item: "blouse", position: 8},
-      a7: {item: "bookmark",position:4},
-      c8: {item:"candy wrapper",position:18},
-      c3: {item:"chalk",position:13},
-      b10: {item:"cookie jar",position:30},
-      b9: {item:"deodorant",position:29},
-      c2: {item:"drill press",position:12},
-      c6: {item:"face wash",position:16},
-      a9: {item:"glow stick",position:2},
-      a4: {item:"hanger",position:7},
-      c10: {item:"leg warmers",position:20},
-      a8: {item:"model car",position:3},     
-      b5: {item:"nail filer",position:25},
-      a1: {item:"needle",position:10},
-      c7: {item:"paint brush",position:17},
-      b4: {item:"photo album",position:24},
-      b3: {item:"picture frame",position:23},
-      a10: {item:"rubber band",position:1},
-      a5: {item:"rubber duck",position:6},
-      c1: {item:"rusty nail",position:11},
-      b2: {item:"sharpie",position:22},
-      c9: {item:"shoe lace",position:19},
-      a6: {item:"shovel",position:5},
-      a2: {item:"stop sign",position:9},
-      c5: {item:"thermometer",position:15},
-      b1: {item:"tyre swing",position:21},
-      b8: {item:"tissue box",position:28},
-      b6: {item:"tooth paste",position:26},
-      c4: {item:"word search",position:14}
+      b7: {item: "bath fizzers", position: 27, weight:3},
+      a3: {item: "blouse", position: 8, weight:1},
+      a7: {item: "bookmark",position:4, weight:1},
+      c8: {item:"candy wrapper",position:18, weight:1},
+      c3: {item:"chalk",position:13, weight:1},
+      b10: {item:"cookie jar",position:30, weight:5},
+      b9: {item:"deodorant",position:29, weight:2},
+      c2: {item:"drill press",position:12, weight:10},
+      c6: {item:"face wash",position:16, weight:3},
+      a9: {item:"glow stick",position:2, weight:2},
+      a4: {item:"hanger",position:7, weight:2},
+      c10: {item:"leg warmers",position:20, weight:3},
+      a8: {item:"model car",position:3, weight:2},     
+      b5: {item:"nail filer",position:25, weight:1},
+      a1: {item:"needle",position:10, weight:1},
+      c7: {item:"paint brush",position:17, weight:2},
+      b4: {item:"photo album",position:24, weight:4},
+      b3: {item:"picture frame",position:23, weight:5},
+      a10: {item:"rubber band",position:1, weight:1},
+      a5: {item:"rubber duck",position:6, weight:2},
+      c1: {item:"rusty nail",position:11, weight:1},
+      b2: {item:"sharpie",position:22, weight:1},
+      c9: {item:"shoe lace",position:19, weight:1},
+      a6: {item:"shovel",position:5, weight:8},
+      a2: {item:"stop sign",position:9, weight:10},
+      c5: {item:"thermometer",position:15, weight:2},
+      b1: {item:"tyre swing",position:21, weight:15},
+      b8: {item:"tissue box",position:28, weight:3},
+      b6: {item:"tooth paste",position:26, weight:2},
+      c4: {item:"word search",position:14, weight:2}
 }
 
 end
@@ -111,6 +111,28 @@ def test_find_bays_and_distance_apart
 result = find_multiple_bays_and_distance(@warehouse_data,["bath fizzers","blouse","bookmark"])
 assert_equal({:bays=>[:b7, :a3, :a7],:distance=>23},result)
 end
+
+
+#------------------------------------------
+# test to return list of bays for multiple items in order that items need to be collected
+
+def test_find_bays_and_picking_order
+  result = find_bays_and_picking_order(@warehouse_data,["shoe lace", "rusty nail", "leg warmers"])
+  assert_equal([:c1, :c9, :c10],result)
+end
+
+# ADDITIONAL FUNCTIONS
+
+#------------------------------------------
+# test to return the weight of all items in a list of bays
+def test_weight_of_items_in_bays
+    result = weight_of_items_in_bays(@warehouse_data,[:c7,:b4,:b3])
+    assert_equal(11,result)
+
+end
+
+
+
 
 
 end
